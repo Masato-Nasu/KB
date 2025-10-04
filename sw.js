9d74a8b074ee46e24620ce94c969bcdb9d2cad21
Masato-Nasu <1bitexist@gmail.com>
2025-10-04 18:36:54 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,21 @@
-self.addEventListener('install', e=>{
-  e.waitUntil(caches.open('graychime-v1').then(c=>c.addAll(['./','./index.html','./manifest.json','./babyface-192.png','./babyface-512.png','./Sparkling Chime Sound.m4a'])));
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('kaleidobaby-v1').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './babyface-192.png',
+        './babyface-512.png'
+      ]);
+    })
+  );
 });
-self.addEventListener('fetch', e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
