ddf13125e881e91c02e4bad9cd3eb67f20daa44b
Masato-Nasu <1bitexist@gmail.com>
2025-10-04 22:07:46 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,21 +1,40 @@
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('kaleidobaby-v1').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json',
-        './babyface-192.png',
-        './babyface-512.png'
-      ]);
-    })
-  );
+const CACHE = 'kaleidobaby-v5';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './babyface-192.png',
+  './babyface-512.png',
+  './Sparkling Chime Sound.m4a'
+];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('activate', (event) => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)));
+    await self.clients.claim();
+  })());
+});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting();
+});
+
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  event.respondWith((async () => {
+    const cached = await caches.match(req, {ignoreSearch: true});
+    if (cached) return cached;
+    try {
+      const res = await fetch(req);
+      return res;
+    } catch (e) {
+      return cached || Response.error();
+    }
+  })());
 });
