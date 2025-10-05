113846bd74206a0d9ca83da5a3332ce45a8b3693
Masato-Nasu <1bitexist@gmail.com>
2025-10-05 19:31:20 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,45 +1,29 @@
-// sw.js v28
-const SW_VERSION = "v28";
-const CACHE = "radiant-" + SW_VERSION;
-
-const ASSETS = [
-  "./",
-  "./index.html?v=28",
-  "./manifest.json?v=28",
-  "./icon-192.png",
-  "./icon-512.png",
-  "./Chime.mp3"
+// sw.js v1 for Silk Orbits
+const CACHE='silk-orbits-v1';
+const ASSETS=[
+  './',
+  './index.html?v=1',
+  './manifest.json?v=1',
+  './icon-192.png',
+  './icon-512.png'
 ];
-
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
   self.skipWaiting();
 });
-
-self.addEventListener("activate", e => {
+self.addEventListener('activate',e=>{
   e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),
   );
   self.clients.claim();
 });
-
-self.addEventListener("fetch", e => {
-  const req = e.request;
-  if (req.mode === "navigate") {
-    e.respondWith(
-      fetch(req).then(r => {
-        const copy = r.clone();
-        caches.open(CACHE).then(c => c.put("./index.html?v=28", copy));
-        return r;
-      }).catch(() => caches.match("./index.html?v=28"))
-    );
+self.addEventListener('fetch',e=>{
+  const req=e.request;
+  if(req.mode==='navigate'){
+    e.respondWith(fetch(req).catch(()=>caches.match('./index.html?v=1')));
     return;
   }
-  e.respondWith(
-    caches.match(req).then(cached => cached || fetch(req).then(r => {
-      const copy = r.clone();
-      caches.open(CACHE).then(c => c.put(req, copy));
-      return r;
-    }))
-  );
+  e.respondWith(caches.match(req).then(cached=>cached || fetch(req).then(r=>{
+    const copy=r.clone(); caches.open(CACHE).then(c=>c.put(req,copy)); return r;
+  })));
 });
