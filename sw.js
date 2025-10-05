bc9a7417647f9a176fe4b48a419eb341b575687d
Masato-Nasu <1bitexist@gmail.com>
2025-10-05 19:17:41 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-// sw.js v26
-const SW_VERSION = "v26";
+// sw.js v27
+const SW_VERSION = "v27";
 const CACHE = "radiant-" + SW_VERSION;
 
 const ASSETS = [
   "./",
-  "./index.html?v=26",
-  "./manifest.json?v=26",
+  "./index.html?v=27",
+  "./manifest.json?v=27",
   "./icon-192.png",
   "./icon-512.png",
   "./Chime.mp3"
@@ -29,9 +29,9 @@ self.addEventListener("fetch", e => {
     e.respondWith(
       fetch(req).then(r => {
         const copy = r.clone();
-        caches.open(CACHE).then(c => c.put("./index.html?v=26", copy));
+        caches.open(CACHE).then(c => c.put("./index.html?v=27", copy));
         return r;
-      }).catch(() => caches.match("./index.html?v=26"))
+      }).catch(() => caches.match("./index.html?v=27"))
     );
     return;
   }
